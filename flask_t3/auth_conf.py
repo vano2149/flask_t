adc44338c232992f143bd875af48335683f95f54
Kal'ko Ivan <vano2149@gmail.com>
2022-03-21 17:21:21 +0300

    Modified auth_conf.py

--- flask_t3/auth_conf.py
+++ flask_t3/auth_conf.py
@@ -1,3 +1,12 @@
 """
 Написать описание модуля.
 """
+from user import User
+from werkzeug.security import safe_str_cmp
+
+users = [
+    User(1, "Alex", "abcdefg"),
+    User(2, "Bob", "qwerty123")
+]
+
+# Продолжить с лек 6
