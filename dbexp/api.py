dff08c5c072b89f2bafafe3021f7068bdbaa81a2
Kal'ko Ivan <vano2149@gmail.com>
2022-03-24 11:36:40 +0300

    Create an flask_t4 folder and an api file, Modified in dbexp/api.py file

--- dbexp/api.py
+++ dbexp/api.py
@@ -6,9 +6,9 @@ flask_restful
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
-#from auth_conf import authenticate, identity
+from auth_conf import authenticate, identity
 import os
-#from user import UserRegisterResourse
+from user import UserRegisterResourse
 
 # Сущность приложения
 app = Flask(__name__)
@@ -32,6 +32,10 @@ class Item(Resource):
     Ресурс, отвечающий за работу с ОДНОЙ товарной единицей
     Реализует CRUD.
     """
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help="Field 'price' required float value!")
+    parser.add_argument('amount', type=int, required=True, help="This field required int value")
+
     def get(self, name):
         """
         GET /item/<staring:name> - возврашает информацию про товар с именем name.
@@ -40,21 +44,41 @@ class Item(Resource):
         if item:
             return {"item", item}, 200
         return{"Error" : "Item with that name not found"}, 404
-    def post():
+
+    def post(self, name):
         """
         POST /item/<string:name> - создает товар с именем name
         """
-        pass
-    def put():
-        """
+        if next(filter(lambda x : x['name'] == name, db),None):
+            return {'Error': f'Item with name {name} already exists!'}, 400
 
-        """
-        pass
-    def delete():
-        """
+        request_body = Item.parser.parse_args()
+        new_item = {"name" : name, "price": request_body["price"], "amount": request_body["amount"]}
+        db.append(new_item)
+        return new_item, 201
 
+    def put(self, name):
         """
-        pass
+        PUT /item/<string:name>
+        """
+        item = next(filter(lambda x : x['name'] == name, db),None)
+        if item:
+            request_body=Item.parser.parse_args()
+            item.update(request_body)
+            return item, 202
+        return {'Error' : f"Item with name {name} not found"}, 404
+
+    def delete(self, name):
+        """
+        DELETE /item/<string:name>
+        """
+        global db
+        start_len = len(db)
+        db = list(filter(lambda x : x["name"] != name, db))
+        if abs(start_len - len(db)) > 0:
+            return {"Message" : "Item successfule deleted!"}, 202
+        return {"Error" : f"Item with that name {name} not found!"}, 404
+
 class ItemCollections(Resource):
     """
     """
@@ -66,4 +90,5 @@ class ItemCollections(Resource):
 api.add_resource(Item,'/item/<string:name>')
 api.add_resource(ItemCollections,'/items')
 if __name__ == '__main__':
+    import table
     app.run(port=8080, debug=True)
