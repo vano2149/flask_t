7e4075bf6afc995c74113b4dc128be367409ae01
Kal'ko Ivan <vano2149@gmail.com>
2022-03-24 11:48:33 +0300

    Modified an table.py file

--- dbexp/table.py
+++ dbexp/table.py
@@ -1,3 +1,18 @@
 """
 Модуль для проверки базы данных к работе.
 """
+"""
+Модуть для подготовки базы данных к работе.
+"""
+import sqlite3
+
+conn = sqlite3.connect("data.db")
+cur = conn.cursor()
+
+
+#Cоздадим таблицу для хранения пользователей.
+create_users = 'CREATE TABLE IF EXISTS user (id INTEGER PRYMARY KEY, username TEXT, password TEXT)'
+cur.execute(create_users)
+
+conn.commit()
+conn.close()
